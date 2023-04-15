64240ccbc25cfd6cce23b33517a88d53cb4e714a
Farzana Hadifa <farzana.hadifa@hashmicro.com>
2023-04-15 08:50:31 +0700

    routes and controller pt, prodi, dosen, mahasiswa, kelas

--- server-app/api/controllers/manageData.js
+++ server-app/api/controllers/manageData.js
@@ -6,10 +6,10 @@ exports.createPT = async(req, res) => {
         const data = req.body;
         const name = data.nama;
 
-        constresult = await dataService.createPT(1,name)
+        const result = await dataService.createPT(1,name)
         res.status(201).send({
             message: "Pendidikan Tinggi is created",
-            data
+            result
         })
     }
     catch(error){
@@ -22,7 +22,7 @@ exports.updatePT = async(req, res) => {
         const data = req.body;
         const name = data.nama;
 
-        constresult = await dataService.createPT(1,  name)
+        const result = await dataService.updatePT(1,  name)
         res.status(200).send({
             message: `Pendidikan Tinggi with id ${req.params.id} is updated`,
             
@@ -44,7 +44,6 @@ exports.deletePT = async(req, res) => {
     }
 }
 
-
 exports.getAllPT = async(req, res) => {
     data = await dataService.getAllPT(1) 
     res.status(200).send({data});
@@ -52,16 +51,258 @@ exports.getAllPT = async(req, res) => {
 
 
 //Prodi
-exports.getAllProdi = async(req, res) =>  {
+exports.createProdi = async(req, res) => {
+    try{
+        const data = req.body;
+        const name = data.nama;
 
-    res.send("Prodi");
+        const result = await dataService.createProdi(1,name)
+        res.status(201).send({
+            message: "Prodi is created",
+            data
+        })
+    }
+    catch(error){
+        
+    }
+}
+
+exports.updateProdi = async(req, res) => {
+    try{
+        const data = req.body;
+        const name = data.nama;
+
+        const result = await dataService.createProdi(1,  name)
+        res.status(200).send({
+            message: `Prodi with id ${req.params.id} is updated`,
+            
+        })
+    }
+    catch(error){
+        
+    }
+}
+
+exports.deleteProdi = async(req, res) => {
+    try{
+        res.status(200).send({
+            message: `Prodi with id ${req.params.id} is deleted`,
+        })
+    }
+    catch(error){
+        
+    }
+}
+
+exports.getAllProdi = async(req, res) => {
+    data = await dataService.getAllProdi(1) 
+    res.status(200).send({data});
 }
 
 
+
 // Dosen
+exports.createDosen = async(req, res) => {
+    try{
+        const data = req.body;
+        const name = data.nama;
+
+        const result = await dataService.createDosen(1,name)
+        res.status(201).send({
+            message: "Dosen is created",
+            data
+        })
+    }
+    catch(error){
+        
+    }
+}
+
+exports.updateDosen = async(req, res) => {
+    try{
+        const data = req.body;
+        const name = data.nama;
+
+        const result = await dataService.createDosen(1,  name)
+        res.status(200).send({
+            message: `Dosen with id ${req.params.id} is updated`,
+            
+        })
+    }
+    catch(error){
+        
+    }
+}
+
+exports.deleteDosen = async(req, res) => {
+    try{
+        res.status(200).send({
+            message: `Dosen with id ${req.params.id} is deleted`,
+        })
+    }
+    catch(error){
+        
+    }
+}
+
+
+exports.getAllDosen = async(req, res) => {
+    data = await dataService.getAllDosen(1) 
+    res.status(200).send({data});
+}
+
 //Mahasiswa
 
+exports.createMahasiswa = async(req, res) => {
+    try{
+        const data = req.body;
+        const name = data.nama;
+
+        const result = await dataService.createMahasiswa(1,name)
+        res.status(201).send({
+            message: "Mahasiswa is created",
+            data
+        })
+    }
+    catch(error){
+        
+    }
+}
+
+exports.updateMahasiswa = async(req, res) => {
+    try{
+        const data = req.body;
+        const name = data.nama;
+
+        const result = await dataService.createMahasiswa(1,  name)
+        res.status(200).send({
+            message: `Mahasiswa with id ${req.params.id} is updated`,
+            
+        })
+    }
+    catch(error){
+        
+    }
+}
+
+exports.deleteMahasiswa = async(req, res) => {
+    try{
+        res.status(200).send({
+            message: `Mahasiswa with id ${req.params.id} is deleted`,
+        })
+    }
+    catch(error){
+        
+    }
+}
+
+
+exports.getAllMahasiswa = async(req, res) => {
+    data = await dataService.getAllMahasiswa(1) 
+    res.status(200).send({data});
+}
+
 //Kelas
+exports.createKelas = async(req, res) => {
+    try{
+        const data = req.body;
+        const name = data.nama;
+
+        const result = await dataService.createKelas(1,name)
+        res.status(201).send({
+            message: "Kelas is created",
+            data
+        })
+    }
+    catch(error){
+        
+    }
+}
+
+exports.updateKelas = async(req, res) => {
+    try{
+        const data = req.body;
+        const name = data.nama;
+
+        const result = await dataService.createKelas(1,  name)
+        res.status(200).send({
+            message: `Kelas with id ${req.params.id} is updated`,
+            
+        })
+    }
+    catch(error){
+        
+    }
+}
+
+exports.deleteKelas = async(req, res) => {
+    try{
+        res.status(200).send({
+            message: `Kelas with id ${req.params.id} is deleted`,
+        })
+    }
+    catch(error){
+        
+    }
+}
+
+
+exports.getAllKelas = async(req, res) => {
+    data = await dataService.getAllKelas(1) 
+    res.status(200).send({data});
+}
+
+
+//Verifier
+exports.createVerifier = async(req, res) => {
+    try{
+        const data = req.body;
+        const name = data.nama;
+
+        const result = await dataService.createVerifier(1,name)
+        res.status(201).send({
+            message: "Verifier is created",
+            data
+        })
+    }
+    catch(error){
+        
+    }
+}
+
+exports.updateVerifier = async(req, res) => {
+    try{
+        const data = req.body;
+        const name = data.nama;
+
+        const result = await dataService.createVerifier(1,  name)
+        res.status(200).send({
+            message: `Verifier with id ${req.params.id} is updated`,
+            
+        })
+    }
+    catch(error){
+        
+    }
+}
+
+exports.deleteVerifier = async(req, res) => {
+    try{
+        res.status(200).send({
+            message: `Verifier with id ${req.params.id} is deleted`,
+        })
+    }
+    catch(error){
+        
+    }
+}
+
+
+exports.getAllVerifier = async(req, res) => {
+    data = await dataService.getAllVerifier(1) 
+    res.status(200).send({data});
+}
+
 
 
 
