50c14b6e738ea2723a551befa65151a2e8cfbe6a
Farzana Hadifa <farzana.hadifa@hashmicro.com>
2023-04-17 11:55:25 +0700

    request response for PT, prodi, matkul, kelas, dosen, mahasiswa

--- server-app/api/controllers/manageData.js
+++ server-app/api/controllers/manageData.js
@@ -35,6 +35,7 @@ exports.updatePT = async(req, res) => {
 
 exports.deletePT = async(req, res) => {
     try{
+        const result = await dataService.deletePT(1);
         res.status(200).send({
             message: `Pendidikan Tinggi with id ${req.params.id} is deleted`,
         })
@@ -72,7 +73,7 @@ exports.updateProdi = async(req, res) => {
         const data = req.body;
         const name = data.nama;
 
-        const result = await dataService.createProdi(1,  name)
+        const result = await dataService.updateProdi(1,  name)
         res.status(200).send({
             message: `Prodi with id ${req.params.id} is updated`,
             
@@ -84,6 +85,7 @@ exports.updateProdi = async(req, res) => {
 }
 
 exports.deleteProdi = async(req, res) => {
+    const result = await dataService.deleteProdi(1)
     try{
         res.status(200).send({
             message: `Prodi with id ${req.params.id} is deleted`,
@@ -123,7 +125,7 @@ exports.updateDosen = async(req, res) => {
         const data = req.body;
         const name = data.nama;
 
-        const result = await dataService.createDosen(1,  name)
+        const result = await dataService.updateDosen(1,  name)
         res.status(200).send({
             message: `Dosen with id ${req.params.id} is updated`,
             
@@ -135,6 +137,7 @@ exports.updateDosen = async(req, res) => {
 }
 
 exports.deleteDosen = async(req, res) => {
+    const result = await dataService.deleteDosen(1)
     try{
         res.status(200).send({
             message: `Dosen with id ${req.params.id} is deleted`,
@@ -174,7 +177,7 @@ exports.updateMahasiswa = async(req, res) => {
         const data = req.body;
         const name = data.nama;
 
-        const result = await dataService.createMahasiswa(1,  name)
+        const result = await dataService.updateMahasiswa(1,  name)
         res.status(200).send({
             message: `Mahasiswa with id ${req.params.id} is updated`,
             
@@ -187,6 +190,7 @@ exports.updateMahasiswa = async(req, res) => {
 
 exports.deleteMahasiswa = async(req, res) => {
     try{
+        const result = await dataService.deleteMahasiswa(1)
         res.status(200).send({
             message: `Mahasiswa with id ${req.params.id} is deleted`,
         })
@@ -201,6 +205,69 @@ exports.getAllMahasiswa = async(req, res) => {
     data = await dataService.getAllMahasiswa(1) 
     res.status(200).send({data});
 }
+
+exports.getMahasiswaById = async(req, res) => {
+    data = await dataService.getMahasiswaById(1) 
+    res.status(200).send({data});
+}
+
+exports.getMahasiswaByKelas = async(req, res) => {
+    data = await dataService.getMahasiswaByKelas(1) 
+    res.status(200).send({data});
+}
+
+//Mata KUliah
+exports.createMataKuliah = async(req, res) => {
+    try{
+        const data = req.body;
+        const name = data.nama;
+
+        const result = await dataService.createMataKuliah(1,name)
+        res.status(201).send({
+            message: "Mata Kuliah is created",
+            data
+        })
+    }
+    catch(error){
+        
+    }
+}
+
+exports.updateMataKuliah = async(req, res) => {
+    try{
+        const data = req.body;
+        const name = data.nama;
+
+        const result = await dataService.updateMataKuliah(1,  name)
+        res.status(200).send({
+            message: `Mata Kuliah with id ${req.params.id} is updated`,
+            
+        })
+    }
+    catch(error){
+        
+    }
+}
+
+exports.deleteMataKuliah = async(req, res) => {
+    try{
+        const result = await dataService.deleteMataKuliah(1)
+        res.status(200).send({
+            message: `MataKuliah with id ${req.params.id} is deleted`,
+        })
+    }
+    catch(error){
+        7
+    }
+}
+
+
+exports.getAllMataKuliah = async(req, res) => {
+    data = await dataService.getAllMataKuliah(1) 
+    res.status(200).send({data});
+}
+
+
 
 //Kelas
 exports.createKelas = async(req, res) => {
@@ -224,7 +291,7 @@ exports.updateKelas = async(req, res) => {
         const data = req.body;
         const name = data.nama;
 
-        const result = await dataService.createKelas(1,  name)
+        const result = await dataService.updateKelas(1,  name)
         res.status(200).send({
             message: `Kelas with id ${req.params.id} is updated`,
             
@@ -237,6 +304,7 @@ exports.updateKelas = async(req, res) => {
 
 exports.deleteKelas = async(req, res) => {
     try{
+        const result = await dataService.deleteKelas(1)
         res.status(200).send({
             message: `Kelas with id ${req.params.id} is deleted`,
         })
@@ -246,6 +314,40 @@ exports.deleteKelas = async(req, res) => {
     }
 }
 
+exports.assignDosen = async(req, res) => {
+    try{
+        const data = req.body;
+        const idKelas = data.idKelas;
+        const idDosen = data.idDosen;
+
+        const result = await dataService.assignDosen(1,idDosen)
+        res.status(200).send({
+            message: `Dosen with id ${idDosen} is assign to class with id ${idKelas}`,
+            data
+        })
+    }
+    catch(error){
+        
+    }
+
+}
+
+exports.assignMahasiswa = async(req, res) => {
+    try{
+        const data = req.body;
+        const idKelas = data.idKelas;
+        const idMahasiswa = data.idMahasiswa;
+
+        const result = await dataService.assignMahasiswa(1,idMahasiswa)
+        res.status(200).send({
+            message: `Mahasiswa with id ${idMahasiswa} is assign to class with id ${idKelas}`,
+            data
+        })
+    }
+    catch(error){
+        
+    }
+}
 
 exports.getAllKelas = async(req, res) => {
     data = await dataService.getAllKelas(1) 
@@ -275,7 +377,7 @@ exports.updateVerifier = async(req, res) => {
         const data = req.body;
         const name = data.nama;
 
-        const result = await dataService.createVerifier(1,  name)
+        const result = await dataService.updateVerifier(1,  name)
         res.status(200).send({
             message: `Verifier with id ${req.params.id} is updated`,
             
@@ -288,6 +390,7 @@ exports.updateVerifier = async(req, res) => {
 
 exports.deleteVerifier = async(req, res) => {
     try{
+        const result = await dataService.deleteVerifier(1)
         res.status(200).send({
             message: `Verifier with id ${req.params.id} is deleted`,
         })
